735ebd2f19e828e7acbf75f07013fe3b58626495
AaronZhong0415 <166473883+AaronZhong0415@users.noreply.github.com>
2024-04-25 19:40:24 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,13 +8,6 @@
 
 
 const urls = [
-	'https://www.surveycake.com/s/2VwoK,
-	'https://www.surveycake.com/s/x9O1y,
-	'https://www.surveycake.com/s/Y8Dmv,
-	'https://www.surveycake.com/s/7dx3N,
-	'https://www.surveycake.com/s/Og94Q,
-	'https://www.surveycake.com/s/qrKWd,
-	'https://www.surveycake.com/s/kzeN0,
-	'https://www.surveycake.com/s/2Vwm3,
-	'https://www.surveycake.com/s/Md7ZQ
+	 'https://www.surveycake.com/s/KnYeN',
+    'https://www.surveycake.com/s/Knxqk'
 ];
